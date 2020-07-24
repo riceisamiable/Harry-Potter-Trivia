3a093f7e9b603502f13459f1a44472443cc7dcef
David Kurz <david.kurz@gmail.com>
2020-07-24 17:03:40 -0400

    added node HTTP server

--- htmlFile.js
+++ htmlFile.js
@@ -1,16 +1,5 @@
 const http = require("http");
-const fs = require('fs').promises;
-
 const requestListener = function (req, res) {
-    fs.readFile("index.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
+    res.writeHead(200);
+    res.end("My first server!");
 };
